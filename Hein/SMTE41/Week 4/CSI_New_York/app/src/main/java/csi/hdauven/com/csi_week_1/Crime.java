14e41ae15f1552823ee3b3e981b01f354e1d319b
Hein Dauven <heindauven@gmail.com>
2016-03-17 17:35:11 +0100

    Week 4 assignment + UX presentations

---
+++ Hein/SMTE41/Week 4/CSI_New_York/app/src/main/java/csi/hdauven/com/csi_week_1/Crime.java
@@ -0,0 +1,9 @@
+package csi.hdauven.com.csi_week_1;
+
+public class Crime {
+
+	public String name;
+	public int bountyInDollars;
+	public String description;
+	
+}
---
+++ Hein/SMTE41/Week 4/CSI_New_York/app/src/main/java/csi/hdauven/com/csi_week_1/Criminal.java
@@ -0,0 +1,28 @@
+package csi.hdauven.com.csi_week_1;
+
+import android.graphics.drawable.Drawable;
+import android.location.Location;
+
+import java.util.ArrayList;
+
+public class Criminal {
+
+	public String name;
+	public String gender;
+	public String description;
+	public int age;
+	
+	public ArrayList<Crime> crimes;
+	
+	public Drawable mugshot;
+	public Location lastKnownLocation;
+	
+	public int getBountyInDollars() {
+		int bounty = 0;
+		for(Crime crime : crimes) {
+			bounty += crime.bountyInDollars;
+		}
+		
+		return bounty;
+	}
+}
