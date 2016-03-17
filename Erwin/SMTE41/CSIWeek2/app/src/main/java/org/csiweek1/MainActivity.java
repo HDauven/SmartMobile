342a664f345788d05e1ff394e2b8945f2ea9d76a
Boudewijns,Erwin E.A.C <e.boudewijns@student.fontys.nl>
2016-03-17 09:21:48 +0100

    Assignment week 3 finished

--- Erwin/SMTE41/CSIWeek2/app/src/main/java/org/csiweek1/MainActivity.java
+++ Erwin/SMTE41/CSIWeek2/app/src/main/java/org/csiweek1/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,5 +24,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Intent i = getIntent();
+        String criminalName = i.getStringExtra("name");
+        TextView textView = (TextView) findViewById(R.id.text_name_value);
+        textView.setText(criminalName);
     }
 }
