f593319a71b28c50c16b871f62b417d7419f2a86
Boudewijns,Erwin E.A.C <e.boudewijns@student.fontys.nl>
2016-03-31 11:49:11 +0200

    Week 5 started

--- Erwin/SMTE41/CSIWeek4/app/src/main/java/org/csiweek1/MainActivity.java
+++ Erwin/SMTE41/CSIWeek4/app/src/main/java/org/csiweek1/MainActivity.java
@@ -2,7 +2,13 @@ package org.csiweek1;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.media.Image;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,7 +21,7 @@ import org.w3c.dom.Text;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public abstract class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
