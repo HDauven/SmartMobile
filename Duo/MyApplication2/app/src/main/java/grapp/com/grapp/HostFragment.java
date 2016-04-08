4b5d82753ab2d47658300979cc8a2055c392230e
Hein Dauven <heindauven@gmail.com>
2016-04-08 11:00:13 +0200

    Added tabbed swipe view

---
+++ Duo/MyApplication2/app/src/main/java/grapp/com/grapp/HostFragment.java
@@ -0,0 +1,39 @@
+package grapp.com.grapp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Hein on 4/8/2016.
+ */
+public class HostFragment extends BackStackFragment {
+    private Fragment fragment;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.host_fragment, container, false);
+        if (fragment != null) {
+            replaceFragment(fragment, false);
+        }
+        return view;
+    }
+
+    public void replaceFragment(Fragment fragment, boolean addToBackstack) {
+        if (addToBackstack) {
+            getChildFragmentManager().beginTransaction().replace(R.id.hosted_fragment, fragment).addToBackStack(null).commit();
+        } else {
+            getChildFragmentManager().beginTransaction().replace(R.id.hosted_fragment, fragment).commit();
+        }
+    }
+
+    public static HostFragment newInstance(Fragment fragment) {
+        HostFragment hostFragment = new HostFragment();
+        hostFragment.fragment = fragment;
+        return hostFragment;
+    }
+}
--- Duo/MyApplication2/app/src/main/java/grapp/com/grapp/MainActivity.java
+++ Duo/MyApplication2/app/src/main/java/grapp/com/grapp/MainActivity.java
@@ -1,13 +1,41 @@
 package grapp.com.grapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private MainPagerAdapter customPagerAdapter;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        customPagerAdapter = new MainPagerAdapter(getSupportFragmentManager());
+
+        // increase this limit if you have more tabs!
+        viewPager.setOffscreenPageLimit(1);
+        viewPager.setAdapter(customPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        if(!BackStackFragment.handleBackPressed(getSupportFragmentManager())){
+            super.onBackPressed();
+        }
+    }
+
+    public void openNewContentFragment(int depth, int fontSize) {
+        HostFragment hostFragment = (HostFragment) customPagerAdapter.getItem(viewPager.getCurrentItem());
+        hostFragment.replaceFragment(ContentFragment.newInstance(depth, fontSize), true);
     }
 }
