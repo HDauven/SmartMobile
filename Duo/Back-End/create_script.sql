017933d253d410d5d2b75fafb0cb61087a2b30d4
Hein Dauven <heindauven@gmail.com>
2016-04-18 23:21:29 +0200

    Refined create script naming scheme

--- Duo/Back-End/create_script.sql
+++ Duo/Back-End/create_script.sql
@@ -14,10 +14,9 @@ drop table users;
 /*	CREATE STATEMENTS											*/
 /*==============================================================*/
 create table users(
-   user_pk int(11) primary key auto_increment,
-   name varchar(50) not null,
-   email varchar(100) not null unique,
-   password varchar(80) not null,
-   salt varchar(10) not null,
+	user_id int(11) primary key auto_increment,
+   user_name varchar(50) not null,
+   user_email varchar(100) not null unique,
+   user__password varchar(80) not null,
    _tc timestamp
 ); 
