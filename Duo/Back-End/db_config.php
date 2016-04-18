5af148ae37d2443e342efec4aff2ddf11d49fe82
Hein Dauven <heindauven@gmail.com>
2016-04-18 23:06:40 +0200

    Added the Database connection variables
    Without the actual variables included of course ;-)

---
+++ Duo/Back-End/db_config.php
@@ -0,0 +1,7 @@
+<?php
+// Database connection variables
+define('DB_USER', "root"); // database user
+define('DB_PASSWORD', "root"); // database password
+define('DB_DATABASE', "startingandroid_db"); // database name
+define('DB_SERVER', "localhost"); // database server
+?>
