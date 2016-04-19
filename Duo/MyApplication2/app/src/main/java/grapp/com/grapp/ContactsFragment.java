c90353559ac9de433845461c3878caeb113a6195
Boudewijns,Erwin E.A.C <e.boudewijns@student.fontys.nl>
2016-04-19 10:45:45 +0200

    Working contactlist

--- Duo/MyApplication2/app/src/main/java/grapp/com/grapp/ContactsFragment.java
+++ Duo/MyApplication2/app/src/main/java/grapp/com/grapp/ContactsFragment.java
@@ -21,6 +21,7 @@ import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v4.widget.CursorAdapter;
 import android.support.v4.widget.SimpleCursorAdapter;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -39,52 +40,33 @@ import java.util.List;
 public class ContactsFragment extends Fragment  {
 
     private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
-    private ListView mContactsList;
-    private List<String> contacts;
-
-    // Request code for READ_CONTACTS. It can be any number > 0.
-    private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 100;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.contacts_layout, null);
 
-        // Gets the ListView from the View list of the parent activity
-        mContactsList = (ListView) view.findViewById(R.id.contact_list);
-        contacts = new ArrayList<>();
+        ListView mContactsList = (ListView) view.findViewById(R.id.contact_list);
+        List<String> contacts = new ArrayList<>();
         Context context = getActivity();
 
-        // Here, thisActivity is the current activity
         if (ContextCompat.checkSelfPermission(context,
                 Manifest.permission.READ_CONTACTS)
                 != PackageManager.PERMISSION_GRANTED) {
 
-            // Should we show an explanation?
             if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
                     Manifest.permission.READ_CONTACTS)) {
-
-                // Show an expanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
             } else {
-
-                // No explanation needed, we can request the permission.
-
                 ActivityCompat.requestPermissions(getActivity(),
                         new String[]{Manifest.permission.READ_CONTACTS},
                         MY_PERMISSIONS_REQUEST_READ_CONTACTS);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
             }
         }
 
 
         ContentResolver cr = getContext().getContentResolver();
-        Cursor phones = getActivity().getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,null,null, null);
+        Cursor phones = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,null,null, null);
+        Log.d("Items count: ", String.format("%d", phones.getCount()));
         while (phones.moveToNext())
         {
             String name = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
@@ -96,29 +78,6 @@ public class ContactsFragment extends Fragment  {
         return view;
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return;
-            }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
-        }
     }
 
-}
