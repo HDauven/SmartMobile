9ce23c1e95c10a980232458cfe7d3f07dfd69573
Hein Dauven <heindauven@gmail.com>
2016-04-22 12:21:08 +0200

    Added modules to groups

--- Duo/MyApplication2/app/src/main/java/grapp/com/grapp/GroupFragment.java
+++ Duo/MyApplication2/app/src/main/java/grapp/com/grapp/GroupFragment.java
@@ -6,7 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import grapp.com.grapp.model.GrappModule;
 
 /**
  * Created by Hein on 4/21/2016.
@@ -24,8 +31,35 @@ public class GroupFragment extends Fragment {
             currentPosition = savedInstanceState.getInt("position");
         }
         View view = inflater.inflate(R.layout.group_layout, container, false);
+
+        GridView gridView = (GridView) view.findViewById(R.id.group_modules);
+
+        List<GrappModule> allItems = getAllItemObject();
+        ModulesAdapter modulesAdapter = new ModulesAdapter(this.getContext(), allItems);
+        gridView.setAdapter(modulesAdapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Start new fragment and pass the selected group using a bundle.
+                Toast.makeText(getContext(), "Module selected", Toast.LENGTH_SHORT).show();
+            }
+        });
         Toast.makeText(view.getContext(), "Selected item: " + String.valueOf(currentPosition), Toast.LENGTH_SHORT).show();
 
         return view;
+    }
+
+    private List<GrappModule> getAllItemObject() {
+        GrappModule grappModule = null;
+
+        List<GrappModule> modules = new ArrayList<>();
+        modules.add(new GrappModule("Chat", "chat"));
+        modules.add(new GrappModule("Calendar", "calendar"));
+        modules.add(new GrappModule("Pictures", "photo"));
+        modules.add(new GrappModule("Ranking", "ranking"));
+        modules.add(new GrappModule("Tournament", "tournament"));
+        modules.add(new GrappModule("Take photo", "camera"));
+
+        return modules;
     }
 }
